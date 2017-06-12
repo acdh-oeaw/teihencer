2457302096e0aec6686dc40f4ecce9410990bc45
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-06-12 12:11:53 +0200

    added a description, removed cbab-legacy things, updated acdh-logo https://redmine.acdh.oeaw.ac.at/issues/8844#note-14

--- teihencer-webapp/metadata.py
+++ teihencer-webapp/metadata.py
@@ -5,7 +5,7 @@ PROJECT_METADATA = {
     'title': 'TEIHencer',
     'author': 'Peter Andorfer',
     'subtitle': 'An enhancing and enrichment service for TEI-Documents',
-    'description': 'text to come',
+    'description': 'A web service/application to enhance and enrich TEI-Documents (or to be more precise to automatically extract/entrich and manually curate entites (places, persons, works) found/tagged in TEI-Documents',
     'github': 'https://github.com/acdh-oeaw/',
     'purpose_de': 'Ein Web Service zur Anreicherung von TEI-Dokumenten.',
     'purpose_en': 'A web service to enhance and enrich TEI-Documents.'}
